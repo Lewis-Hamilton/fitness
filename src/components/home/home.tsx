acfa3a667ff5626b069b91d224c2863b4799c2ad
Lewis <hamiltonjlewis@gmail.com>
2022-02-05 16:13:29 -0800

    simple working countdown timer

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Home() {
-  return <p>home</p>;
+  const [duration, setDuration] = useState(60);
+
+  const timer = () => {
+    if (duration <= 60) {
+      setInterval(() => {
+        setDuration((duration) => duration - 1);
+      }, 1000);
+    }
+  };
+
+  return (
+    <>
+      <p>{duration}</p>
+      <button onClick={timer}>Start</button>
+    </>
+  );
 }
